ea09f4b393e0cc954395c3ab8e0548924f3ac625
Daniel McClean <dmcclean780@gmail.com>
2024-01-15 13:03:30 +0000

    Added Falling Sand Page with sand drawing default

---
+++ public/FallingSand/DrawingData.js
@@ -0,0 +1,9 @@
+class DrawingData{
+    constructor(rect, clientX, clientY){
+        this.rect=rect;
+        this.clientX=clientX;
+        this.clientY=clientY;
+    }
+}
+
+export{DrawingData};
--- public/indexHTMLScript.js
+++ public/indexHTMLScript.js
@@ -1,3 +1,7 @@
 function goToGameOfLife(){
     window.location.href=("GameOfLife/index.html");
 };
+
+function goToFallingSand(){
+    window.location.href=("FallingSand/index.html");
+};
